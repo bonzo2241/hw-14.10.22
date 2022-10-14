738003d670e4d793b46765b2f7f6e99022cccce7
bonzo2241 <114671675+bonzo2241@users.noreply.github.com>
2022-10-14 09:51:04 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,10 +1,15 @@
-r=int(input("Введите количество чисел"))
+c=0
+r=int(input("Введите количество чисел в первом массиве"))
 A = [0] * r
-k=-100000000000000000000000000000000000000000000000000000000000000
 for i in range (r):
     print(i+1, "Введите число:",)
     A[i]=float(input())
-    if A[i]>k:
-        k=A[i]
-print(k)
-
+g=int(input("Введите количество чисел во втором массиве"))
+B = [0] * g
+for j in range (g):
+    print(j+1, "Введите число:",)
+    B[j]=float(input())
+for i in range(r):
+    for j in range(r):
+        if A[i] == B[j]:
+            print(A[i])
